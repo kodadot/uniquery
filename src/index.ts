6aa0211ff6b748f7362da418203c0f413c28c055
Viki Val <viktorko99@gmail.com>
2023-03-09 11:54:44 +0100

    :bug: forgot to export extendFields

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import getClient from './clients/factory'
 import SquidClient from './clients/SquidClient'
 import getUrl from './indexers'
+
+export { extendFields } from './clients/defaults'
 export * from './rest'
 
 export { SquidClient, getClient, getUrl }
