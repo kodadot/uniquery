53ccbc0a7d565ebdb8a2c36d24b861c1d15baeb6
Viki Val <viktorko99@gmail.com>
2023-03-13 14:19:56 +0100

    :tada: fetch works <3

--- src/clients/SquidClient.ts
+++ src/clients/SquidClient.ts
@@ -163,7 +163,7 @@ class SquidClient implements AbstractClient<SquidCollection, SquidNFT> {
   fetch<D>(query: GraphQuery): Promise<GraphLike<D>> {
     const baseURL = getUrl(this.prefix)
     const opts = getOptions({ query, baseURL, path: '' })
-    return $fetch<GraphLike<D>>(GRAPHQL_PATH, opts)
+    return $fetch<GraphLike<D>>(baseURL, opts)
   }
 }
 
