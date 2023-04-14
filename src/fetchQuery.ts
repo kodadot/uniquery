b422a3b7b50cf91c502611dd82e7f74446b93503
Viki Val <viktorko99@gmail.com>
2023-04-14 21:01:10 +0200

    :wastebasket: unused

--- src/fetchQuery.ts
+++
@@ -1,5 +0,0 @@
-fetch<D>(query: GraphQuery): Promise<GraphLike<D>> {
-  const baseURL = getUrl(this.prefix)
-  const opts = getOptions({ query, baseURL, path: '' })
-  return $fetch<GraphLike<D>>(baseURL, opts)
-}
