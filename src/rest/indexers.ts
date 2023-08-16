9b20fab2356120f2aa37d4cecefb25b867239e7c
Viki Val <viktorko99@gmail.com>
2023-08-16 16:52:09 +0300

    :zap: enable rest route for all chains

--- src/rest/indexers.ts
+++ src/rest/indexers.ts
@@ -1,13 +1,6 @@
+import { INDEXERS } from '@kodadot1/static'
 import { Prefix } from './types'
 
-export const indexers: Record<Prefix, string> = {
-  ksm: 'https://squid.subsquid.io/rubick/v/008',
-  bsx: 'https://squid.subsquid.io/snekk/v/005',
-  snek: 'https://squid.subsquid.io/snekk/v/004',
-  movr: 'https://squid.subsquid.io/antick/v/001-rc0',
-  glmr: 'https://squid.subsquid.io/click/v/002'
-}
+export const getUrl = (chain: Prefix | ''): string => INDEXERS[chain]
 
-export const getUrl = (chain: Prefix | ''): string => indexers[chain]
-
-export const getAvailableChains = (): Prefix[] => Object.keys(indexers) as Prefix[]
+export const getAvailableChains = (): Prefix[] => Object.keys(INDEXERS) as Prefix[]
