3299bcbd12ca4f235be7987bfa49218df2e24986
Viki Val <viktorko99@gmail.com>
2024-10-04 15:39:46 +0200

    app: local in src/rest/indexers.ts

--- src/rest/indexers.ts
+++ src/rest/indexers.ts
@@ -1,7 +1,16 @@
 import { INDEXERS } from '@kodadot1/static'
-import { Prefix } from './types'
+import { Prefix } from '../types'
 
-export const getUrl = (chain: Prefix | ''): string => INDEXERS[chain]
+export const getUrl = (chain: Prefix | ''): string => {
+  switch (chain) {
+    case 'local': {
+      return 'http://localhost:4350/graphql'
+    }
+    default: {
+      return INDEXERS[chain]
+    }
+  }
+}
 
 export const getAvailableChains = (): Prefix[] =>
   Object.keys(INDEXERS) as Prefix[]
