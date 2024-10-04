33ea6b174b003316a4a4c464f3a9ad12969f725c
Viki Val <viktorko99@gmail.com>
2024-10-04 15:46:54 +0200

    :technologist: reexport Prefix from @kodadot1/static

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import getClient from './clients/factory'
 import SquidClient from './clients/SquidClient'
 import getUrl from './indexers'
 
+export type { Prefix } from '@kodadot1/static'
 export { extendFields } from './clients/defaults'
 export { fetchQuery, graphFetch } from './indexers'
 export * from './rest'
