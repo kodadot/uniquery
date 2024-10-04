458a6e998308ac9a4427f422e0f59f4b90c871c7
Viki Val <viktorko99@gmail.com>
2024-10-04 15:39:46 +0200

    app: local in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import getUrl from './indexers'
 
 export { extendFields } from './clients/defaults'
 export { fetchQuery, graphFetch } from './indexers'
+export { Prefix } from './types'
 export * from './rest'
 
 // eslint-disable-next-line unicorn/prefer-export-from
