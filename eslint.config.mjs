12093abbff3d21a8753e02cc0270b690bc428367
roiLeo <medina.leo42@gmail.com>
2024-10-16 14:11:00 +0200

    🔧 eslint config mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
 import unjs from "eslint-config-unjs"
 
 export default unjs({
-	ignores: [
-		"dist",
-		"node_modules"
-	],
-	rules: {
-    	"space-before-function-paren": "off",
-    	"unicorn/filename-case": "off",
-    	"unicorn/no-array-callback-reference": "off"
-	},
+  ignores: [
+    "dist",
+    "node_modules"
+  ],
+  rules: {
+    "space-before-function-paren": "off",
+    "unicorn/filename-case": "off",
+    "unicorn/no-array-callback-reference": "off"
+  },
 })
